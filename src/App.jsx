8f595542ee3d7c93e4438b7826b506654721a868
omotayo daniel ukuhor <dani3lsavag3@gmail.com>
2023-09-24 00:27:13 +0100

    frontend basic setup : trial

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef, useState, useImperativeHandle } from "react";
+import socket from "socket.io-client";
+import Peer  from "simple-peer";
+
+import { LocalVideo,RemoteVideo } from "./components/video";
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const [thereIsRemoteVideo, setRemoteVideo] = useState(true)
+  const localVideoRef = useRef(null);
+  const remoteVideoRef = useRef(null);
+  let vidStream
+  useEffect(function(){
+    navigator.mediaDevices.getUserMedia({video:true,audio:true})
+    .then(stream=>{
+      localVideoRef.current.srcObject = stream;
+      vidStream = stream
+    })
+  });
+
+  function handleStartCall(){
+    let peer = new Peer({initiator:true,trickle:false, stream:vidStream});
+
+    peer.on("signal",function(data){
+        socket.emit("offer",JSON.stringify(data));
+    });
+
+    peer.on("stream",function(stream){
+      remoteVideoRef.current.srcObject = stream;
+    });
+
+    socket.emit("join",'room-1');
+
+    socket.on("answer",function(data){
+      peer.signal(JSON.parse(data))
+    })
+
+  }
+
+  function handleJoinCall(){
+    let peer = new Peer({initiator:false,trickle:false, stream:vidStream});
+
+    peer.on("signal",function(data){
+      socket.emit("answer",JSON.stringify(data));
+    });
+
+    peer.on("stream",function(stream){
+      remoteVideoRef.current.srcObject = stream;
+    });
+
+    socket.emit("join",'room-1');
+
+    socket.on("offer",function(data){
+      peer.signal(JSON.parse(data))
+    })
+
+  }
+
+  return <div>
+            <LocalVideo ref={localVideoRef} />
+            { thereIsRemoteVideo && <RemoteVideo ref={remoteVideoRef} /> }
+            <button onClick={handleStartCall} > Start Call </button>
+            <button onClick={handleJoinCall} > Join Call </button>
+        </div>
 }
+
+
 
 export default App
